ae79383b03a6067c78ace0d69b803d5a7d5dd711
digis <digis@LAPTOP-AD7LFKO5>
2025-05-21 15:55:43 -0600

    Estado by pais

--- src/main/java/com/digis01/FNolascoAPIMovies/Controller/MovieController.java
+++ src/main/java/com/digis01/FNolascoAPIMovies/Controller/MovieController.java
@@ -1,23 +1,56 @@
 package com.digis01.FNolascoAPIMovies.Controller;
 
-import ch.qos.logback.core.model.Model;
+import com.digis01.FNolascoAPIMovies.ML.Result;
+import com.digis01.FNolascoAPIMovies.ML.Usuario;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
 
 
 @Controller
 @RequestMapping("/Movies")
 public class MovieController {
     
+    private RestTemplate restTemplate = new RestTemplate();
+    private String token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNTI2YzYwMGEwNTc4ZWI0OGRiZTg3ZjkzZDhlOTQ2NyIsIm5iZiI6MTc0NzMzNzY5Ny44NDcwMDAxLCJzdWIiOiI2ODI2NDFlMTg0NTg1Yzk5MTQzMTU5OTAiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.zIyxI1UbM3O19KDdoYvHVL0A6KCRzzwdF9aQn-uBzLM";
+    
     @GetMapping
     public String Index(Model model){
         return "MovieIndex";
     }
-    
-    @GetMapping("/login")
-    public String LogIn(){
+
+            
+    @GetMapping
+    public String LogIn(@ModelAttribute Usuario usuario, Model model){
+        model.addAttribute("model", model);
         return "UserLogin";
+    }
+    
+    @PostMapping("/login")
+    public String LogIn(@ModelAttribute Usuario usuario, Model model, HttpSession session){
+        Result result = new Result();
+        try{
+            HttpHeaders httpHeaderToken = new HttpHeaders();
+            httpHeaderToken.setBearerAuth(token);
+            HttpEntity<String> entity = new HttpEntity<>(httpHeaderToken);
+            
+            
+        }catch(Exception ex){
+            result.correct = false;
+            result.errorMessage = ex.getLocalizedMessage();
+            result.ex = ex;
+        }
+        
+        
+        
+        return null;
     }
     
 }
---
+++ src/main/java/com/digis01/FNolascoAPIMovies/ML/Usuario.java
@@ -0,0 +1,13 @@
+package com.digis01.FNolascoAPIMovies.ML;
+
+import java.util.List;
+
+public class Usuario {
+    
+    private String name;
+    private String password;
+    private boolean succes;
+    private String guest_session_id;
+    private String expires_at;
+    private String request_token;
+}
